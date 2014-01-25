19238949ebd69a3ed94348aefc7c51212a9730ff
tmanolescu <t.mann.ro@gmail.com>
2014-01-25 23:55:58 +0200

    curatit css
    curatit css

---
+++ prologue/meniu.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Register</title>
+	<link rel="stylesheet" href="css/mobile.css">
+</head>
+<body>
+	<div class="header">
+		<div class="buttons">
+			<div class="home">
+				<a href="#"><img src="images/home.jpeg" alt="home" width='49px' height='40'>
+				</a>
+			</div>
+			<div class="title"><h1>Meniu</h1></div>
+			<div class="menu">
+				<a href="#"><img src="images/menu.png" alt="menu"></a>
+			</div>
+			
+		</div>
+	</div>
+	<div class="main">
+	<ul>
+		<li><a href="#">Alerta produs</a><img src="images/next.png" alt="next"></li>
+		<li><a href="#">Cumparaturi recurente</a><img src="images/next.png" alt="next"></li>
+		<li><a href="#">Produsul zilei</a><img src="images/next.png" alt="next"></li>
+		<li><a href="#">Setari</a><img src="images/next.png" alt="next"></li>
+	</ul>
+	</div>
+</body>
+</html>
