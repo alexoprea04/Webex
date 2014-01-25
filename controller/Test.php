9ade7d8aa0592c7cc7c2687b0649394109100ae9
ionut-milea <cosm23@yahoo.com>
2014-01-25 18:05:32 +0200

    db connection
    available db connection under controller $this->db

--- controller/Test.php
+++ controller/Test.php
@@ -4,6 +4,14 @@ class Test_Controller extends Base_Controller {
 
     public function index() {
 
+        //test db connection
+        $random = $this->db->prepare('
+                    SELECT 1');
+        $random->execute();
+        $product = $random->fetchAll(PDO::FETCH_ASSOC);
+        
+
+
         //add var to view
         $this->addVar('kkk', 'kkk value');
     }
