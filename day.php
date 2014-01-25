f8fca17eaaf6f0d198574e21c1147555161dfbea
alexoprea04 <alexandru.oprea86@gmail.com>
2014-01-26 00:36:24 +0200

    Fix css header
    css

--- day.php
+++ day.php
@@ -2,19 +2,19 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Register</title>
-	<link rel="stylesheet" href="css/mobile.css">
+	<title>Product</title>
+	<link rel="stylesheet" href="prologue/css/mobile.css">
 </head>
 <body>
 	<div class="header">
 		<div class="buttons">
 			<div class="home">
-				<a href="#"><img src="images/home.jpeg" alt="home" width='49px' height='40'>
+				<a href="#"><img src="prologue/images/home.jpeg" alt="home" width='49px' height='40'>
 				</a>
 			</div>
-			<div class="title"><h1>Meniu</h1></div>
+			<div class="title"><h1>Product</h1></div>
 			<div class="menu">
-				<a href="#"><img src="images/menu.png" alt="menu"></a>
+				<a href="#"><img src="prologue/images/menu.png" alt="menu"></a>
 			</div>
 
 		</div>
