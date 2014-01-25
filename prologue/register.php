ad195eabb6deeff08b69dfe3fcb305a0ae69c36c
tmanolescu <t.mann.ro@gmail.com>
2014-01-25 20:17:15 +0200

    minor updates site
    v

---
+++ prologue/register.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Register</title>
+</head>
+<body>
+	<div class="menu">
+		<h1>Register/Login</h1>
+	</div>
+</body>
+</html>
