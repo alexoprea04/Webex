f396d4976d4dc22a6e976226d657bc7cb313d0f3
wrathd1 <andrei.nechifor@gmail.com>
2014-01-25 19:58:04 +0200

    index

--- index.php
+++ index.php
@@ -5,8 +5,14 @@ ini_set('display_errors', 1);
 
 require_once('include/autoloader.php');
 
-$class = $_GET['class'] . '_Controller';
-$method = $_GET['method'];
+if(!isset($_GET['class'])) {
+	$class = 'Home';
+	$method = 'index';	
+}
+else {
+	$class = $_GET['class'] . '_Controller';
+	$method = $_GET['method'];
+}
 
 $classObj = new $class();
 $classObj->setCalledClass($class);
